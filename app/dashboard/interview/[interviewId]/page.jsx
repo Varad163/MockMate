71c47c73900dd603bb9cd3b3db0ca77a7f3eb8d6
VARADRAJ GHOLAP <varadgholap163@gmail.com>
2024-12-24 00:02:01 +0530

    correcting interviewId.jsx

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -5,7 +5,20 @@ import { db } from '@/utils/db'
 import { eq } from 'drizzle-orm'
 
 function Interview({ params }) {
-   console.log(params);
+
+
+    useEffect(() => {
+        console.log(params.InterviewId)
+        GetInteriewDetails();
+    }, [])
+
+    const GetInteriewDetails = async () => {
+        const result = await db.select().from(MockInterview)
+            .where(eq(MockInterview.mockId, params.InterviewId))
+
+        console.log(result);
+    }
+    GetInteriewDetails();
 
     return (
         <div>Interview</div>
